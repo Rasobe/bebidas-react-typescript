1aa71987d92abb137b746cd6872ad2258a3696a2
rsolano <raul.solano@movicoders.com>
2024-10-30 08:54:15 +0100

    main: header done with custom tailwind bg-header. Done layout

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import IndexView from "./views/IndexView";
 import FavoritesView from "./views/FavoritesView";
+import Layout from "./layouts/Layout";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<IndexView />} />
-        <Route path="/favoritos" element={<FavoritesView />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<IndexView />} index />
+          <Route path="/favoritos" element={<FavoritesView />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/views/IndexView.tsx
+++ src/views/IndexView.tsx
@@ -1,9 +1,7 @@
-import Header from "../components/Header";
-
 export default function IndexView() {
   return (
     <>
-      <Header />
+      <h1>Inicio</h1>
     </>
   );
 }
